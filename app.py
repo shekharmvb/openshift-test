5164102dd1fb04c905735df63de0f7fca54b8d98
shekharmvb <40837123+shekharmvb@users.noreply.github.com>
2023-02-22 23:27:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,7 @@
-from flask import Flask
 import os
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
+# Get the value of the secret named "message"
 message = os.environ.get('message')
 
 # Display the message
-    return "Hello" + message
-
-if __name__ == '__main__':
-    port = os.environ.get('FLASK_PORT') or 8080
-    port = int(port)
-
-    app.run(port=port,host='0.0.0.0')
-
+print('Hello ' + message)
